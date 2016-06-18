e409f04f5cf6db654d66987dbac884905af97605
TylerChang <TylerChangMore@icloud.com>
2016-06-18 20:42:42 +0800

    Added qrcode package

--- app/components/employees/group/members.jsx
+++ app/components/employees/group/members.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Member from './member';
+import QRCode from 'qrcode.react';
 
 export default React.createClass({
     propTypes: {
@@ -16,6 +17,7 @@ export default React.createClass({
                 <Member memberName="Tyler" memberRole={this.props.memberRole}/>
                 <Member memberName="Tyler" memberRole={this.props.memberRole}/>
                 <Member memberName="Tyler" memberRole={this.props.memberRole}/>
+                <QRCode value="http://firmament.com/123456789" size={80}/>
             </div>
         );
     }
